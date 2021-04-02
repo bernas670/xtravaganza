571b348ecc9d4c29fe316d8c86802ed739462c1c
Claudia <up201604832@fe.up.pt>
2021-04-02 22:12:33 +0100

    Enemy movement: enemy moving towards player; characters stat

---
+++ Assets/Scripts/Characters/Stat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* System.Serializable to make it visible in inspector */
+[System.Serializable]
+public class Stat
+{
+    [SerializeField] private int _value;
+
+    [SerializeField] private List<int> _modifiers = new List<int>();
+
+    public int getValue(){
+        return _value;
+    }
+
+    public int getValueWithModifiers(){
+        int finalValue = _value;
+        foreach(int v in _modifiers)
+            finalValue += v;
+
+        return finalValue;
+    }
+
+    public void setValue(int value){
+        _value = value;
+    }
+
+    public void addModifier(int modifier){
+        if(modifier != 0)
+            _modifiers.Add(modifier);
+    }
+
+    public void removeModifier(int modifier){
+        if(modifier != 0)
+            _modifiers.Remove(modifier);
+    }
+    
+}
