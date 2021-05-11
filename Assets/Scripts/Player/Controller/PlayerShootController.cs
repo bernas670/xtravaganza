561423f04b09a86ce2051d75d97d5d981d4d3a61
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-05-11 10:09:02 +0100

    Move around scene elements; Add ammo count ui

--- Assets/Scripts/Player/Controller/PlayerShootController.cs
+++ Assets/Scripts/Player/Controller/PlayerShootController.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerShootController : Shooter
 {
     [SerializeField] protected Camera cam;
-    
-    void Awake(){
+    public TextMeshProUGUI bulletsText;
+
+    void Awake()
+    {
         setPoV(cam.transform);
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        bulletsText.text = string.Format("ammo: {0}", fireWeapon.getClipValue());
     }
 
     void Update()
-    {   
-        if(Input.GetButton("Fire1") && fireWeapon.getClipValue()>0){
+    {
+        if (Input.GetButton("Fire1") && fireWeapon.getClipValue() > 0)
+        {
             fireWeapon.shoot(this);
+            UpdateText();
         }
-        else if(Input.GetButton("Fire2")){ 
+        else if (Input.GetButton("Fire2"))
+        {
             meleeWeapon.shoot(this);
         }
-        else if(Input.GetKey(KeyCode.R)){
+        else if (Input.GetKey(KeyCode.R))
+        {
             fireWeapon.reload();
+            UpdateText();
         }
     }
 
