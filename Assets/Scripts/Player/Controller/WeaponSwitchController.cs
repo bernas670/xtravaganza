dd7f5f30045c36650bd30efcd750b31bd7e87ed1
BernardoCMoreira <up201604014@fe.up.pt>
2021-05-21 15:45:43 +0100

    implemented weapon switch and add a new weapon

---
+++ Assets/Scripts/Player/Controller/WeaponSwitchController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitchController : MonoBehaviour
+{   
+    public int selectedWeapon = 0;
+    void Start(){
+        SelectWeapon();
+    }
+
+    void Update(){
+        int previousWeapon = selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f){
+            Debug.Log("Changed weapon");
+            if(selectedWeapon>= transform.childCount - 1)
+                selectedWeapon = 0;
+            else 
+                selectedWeapon++;
+        }
+        
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f){
+            if(selectedWeapon<= 0)
+                selectedWeapon = transform.childCount - 1;
+            else 
+                selectedWeapon--;
+        }
+
+        // If selected weapon changed, call selectweapon method to perform the weapon change
+        if(previousWeapon != selectedWeapon)
+            SelectWeapon();
+    }
+
+    void SelectWeapon(){
+        int i=0;
+        foreach(Transform weapon in transform){
+            
+            // if the weapon is selected set true, if it's not set false
+            weapon.gameObject.SetActive(i == selectedWeapon);
+            i++;
+        }
+    }
+}
