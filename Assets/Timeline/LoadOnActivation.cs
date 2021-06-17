2919e5646aea48c508133ce8e4184c99bf0364d4
Cláudia Mamede <up201604832@fe.up.pt>
2021-06-17 11:41:19 +0100

    fix transitions w/ menu; not async load scene

--- Assets/Timeline/LoadOnActivation.cs
+++ Assets/Timeline/LoadOnActivation.cs
@@ -9,25 +9,35 @@ public class LoadOnActivation : MonoBehaviour
     {        
         GameObject player = GameObject.Find("Player");
 
-        StartCoroutine(LoadYourAsyncScene());
+        //StartCoroutine(LoadAsyncScene());
+        string pathToScene = SceneUtility.GetScenePathByBuildIndex(2);
+        string sceneName = System.IO.Path.GetFileNameWithoutExtension(pathToScene);
+
+        SceneManager.LoadScene(sceneName);
         
         player.transform.position = new Vector3(-53, 38, -207);
 
         MovementController mController = player.GetComponent<MovementController>();
         mController.enabled = true;
+
+        Debug.Log("segunda cena");
     }
 
-    IEnumerator LoadYourAsyncScene()
+    IEnumerator LoadAsyncScene()
     {
         string pathToScene = SceneUtility.GetScenePathByBuildIndex(1);
         string sceneName = System.IO.Path.GetFileNameWithoutExtension(pathToScene);
 
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-   
+
         // Wait until the asynchronous scene fully loads
         while (!asyncLoad.isDone)
         {
             yield return null;
         }
+
+        Debug.Log("load scene finish");
     }
+
+
 }
