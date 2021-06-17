8fdbc92c6d1817660f86b546db228dc41d7c901a
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-06-17 01:24:31 +0100

    Fix bus name bug; Fix conflicts; Add new sounds to ezisting buses

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -11,16 +11,14 @@ public class OptionsMenu : MonoBehaviour
     float _sfxVol = 0.5f;
     float _mouseSens = 5f;
 
-    // TODO: find out what are the buses' names so we can control sound independently
-
     void Awake()
     {
         _musicVol = PlayerPrefs.GetFloat("music_volume", 0.5f);
         _sfxVol = PlayerPrefs.GetFloat("sfx_volume", 0.5f);
         _mouseSens = PlayerPrefs.GetFloat("mouse_sensitivity", 5f);
 
-        _music = FMODUnity.RuntimeManager.GetBus("bus:/GSFX");
-        _sfx = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        _music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        _sfx = FMODUnity.RuntimeManager.GetBus("bus:/GSFX");
     }
 
     void Start()
